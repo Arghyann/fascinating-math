f91b3e0276d01293dbaeb33ffcbdb02a4101f145
Arghyann <144401510+Arghyann@users.noreply.github.com>
2024-06-02 02:18:14 +0530

    code runner

--- math/fourier/epicycles/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-from epicycloid import EpicycloidAnimator
-from fourierTransform import dft
-# Create an instance of EpicycloidAnimator
-
-
-epicycloid_animator = EpicycloidAnimator(num_vectors=100, total_time=10)
-
-# Start animation
-epicycloid_animator.animate()
