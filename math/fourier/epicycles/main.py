e9341da632071ce457dff502f65b75ed108a5ef0
Arghyann <144401510+Arghyann@users.noreply.github.com>
2024-06-03 04:12:21 +0530

    changed path

--- math/fourier/epicycles/main.py
+++ math/fourier/epicycles/main.py
@@ -1,8 +1,8 @@
 from epicycloid import EpicycloidAnimator
-import numpy as np
+
 # Create an instance of EpicycloidAnimator
 circles=False#change to show circles
-pathToFile=r"D:\fascinating-math\math\fourier\epicycles\output\test1.txt"
+pathToFile=r"D:\projects\python\fascinating-math\math\fourier\epicycles\output\test1.txt"
 epicycloid_animator = EpicycloidAnimator(num_vectors=100, total_time=1,path=pathToFile,show_circles=circles)
 
 # Start animation
