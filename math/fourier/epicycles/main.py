bf15b115b4f4bb5a3df202e3e9f4c48f0b93755c
Arghyann <144401510+Arghyann@users.noreply.github.com>
2024-06-04 04:21:28 +0530

    added sample images for fun

--- math/fourier/epicycles/main.py
+++ math/fourier/epicycles/main.py
@@ -1,10 +1,10 @@
 from epicycloid import EpicycloidAnimator
 
 # Create an instance of EpicycloidAnimator
-circles=False#change to show circles
-pathToFile=r"D:\projects\python\fascinating-math\math\fourier\epicycles\output\test1.txt"
-zoom=False
-epicycloid_animator = EpicycloidAnimator(num_vectors=30, total_time=1,path=pathToFile,show_circles=circles,zoom=zoom)
+circles=False                       #change to show circles
+pathToFile=r"D:\projects\python\fascinating-math\math\fourier\epicycles\sampleImages\fourier.svg"
+zoom=False                          #change to enable or disable zoom
+epicycloid_animator = EpicycloidAnimator(num_vectors=100, total_time=1,path=pathToFile,show_circles=circles,zoom=zoom)
 
 # Start animation
 epicycloid_animator.animate()
