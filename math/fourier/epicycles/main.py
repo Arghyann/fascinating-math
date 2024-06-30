f617f4d0cab0a3341abc9d0921b2586753de3579
Arghyann <144401510+Arghyann@users.noreply.github.com>
2024-06-30 19:41:49 +0530

    requirements.txt added

--- math/fourier/epicycles/main.py
+++ math/fourier/epicycles/main.py
@@ -1,10 +1,10 @@
 from epicycloid import EpicycloidAnimator
 
 # Create an instance of EpicycloidAnimator
-circles=False                       #change to show circles
-pathToFile=r"D:\projects\python\fascinating-math\math\fourier\epicycles\output\texts\test1.txt"
-zoom=False                         #change to enable or disable zoom
-epicycloid_animator = EpicycloidAnimator(num_vectors=100, total_time=1,path=pathToFile,show_circles=circles,zoom=zoom)
+circles=False                     #change to show circles
+pathToFile=r"D:\projects\python\fascinating-math\math\fourier\epicycles\sampleImages\lion.svg"
+zoom=True                        #change to enable or disable zoom
+epicycloid_animator = EpicycloidAnimator(num_vectors=50, total_time=1,path=pathToFile,show_circles=circles,zoom=zoom)
 
 # Start animation
 epicycloid_animator.animate()
